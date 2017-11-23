d91fabfed570a5f4e97adbdef05d9e40c1aac7c6
Hugo.DELTOUR <hugo.deltour@viacesi.fr>
2017-11-23 09:17:07 +0100

    MAJ main

---
+++ src/MAIN/Main.java
@@ -0,0 +1,17 @@
+package MAIN;
+
+import CAD.CAD;
+import Controller.Controller;
+import Model.Model;
+import View.Vue;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Model model = new Model();
+		Vue vue = new Vue();
+		Controller control = new Controller(model,vue);
+		CAD CAD = new CAD(model);
+		
+	}
+}
