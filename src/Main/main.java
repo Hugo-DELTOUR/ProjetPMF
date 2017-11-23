72e65f01e234c5c95bd8822031f971689ac55b3b
Eager (Emilien) <emilien.pons@viacesi.fr>
2017-11-23 09:28:43 +0100

    MAJ V1 (Tout fichiers)
    Combinaison de la vue + Maj réussie des informations affichées

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,17 +1,16 @@
-package CAD;
+package Main;
 
-import java.util.Observable;
-import java.util.Observer;
-
-import Controler.Controler;
+import CAD.Acq_Data;
+import Controler.Controller;
 import Model.Model;
+import View.Vue;
 
 public class main {
 
 	public static void main(String[] args) throws Exception {
 		Model model = new Model();
-		
-		Controler control = new Controler(model);
+		Vue vue = new Vue();
+		Controller control = new Controller(model,vue);
 		Acq_Data CAD = new Acq_Data(model);
 		
 	}
---
+++ src/View/Vue.java
@@ -0,0 +1,183 @@
+package View;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Vue extends JFrame implements IVue, ActionListener{
+
+
+	private float tempInt;
+	private float tempExt;
+	private float humidité;
+	private JLabel imgPorteOuverte = new JLabel(new ImageIcon("porte ouverte.jpg"));
+	private JLabel imgPorteFermée = new JLabel(new ImageIcon("porte fermée.jpg"));
+	private JLabel imgAlerte = new JLabel(new ImageIcon("gif alerte.gif"));
+	private JLabel labelTempInt = new JLabel("Température intérieure(en °):");
+	private JLabel labelTempExt = new JLabel("Température extérieure(en °):");
+	private JLabel labelHumidité = new JLabel("Humidité (en %):");
+	private JLabel labelDerTempExt = new JLabel();	
+	private JLabel labelDerTempInt = new JLabel();
+	private JLabel labelDerHum = new JLabel();
+	private JLabel condensation = new JLabel("Risque de condensation");
+	private JLabel température = new JLabel("Choix de la température:");
+	private JTextField choixTemp = new JTextField("", 20);
+	private JButton bGo = new JButton("Go");
+	private Graphics graph;
+	private Observable observable;
+	private Boolean pointRosée = true;
+	private Boolean porte = true ;
+	
+	public Vue(){
+		
+		JFrame fenetre = new JFrame();
+		JPanel pan = new JPanel();
+		JPanel pan2 = new JPanel();
+		JPanel pan3 = new JPanel();
+		
+		fenetre.setTitle("Projet PMF");
+		fenetre.setSize(700, 500);
+		fenetre.setVisible(true);
+		fenetre.setLocationRelativeTo(null);
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setLayout(new BorderLayout());
+		fenetre.setResizable(false);
+		
+		pan.setLayout(new FlowLayout());
+		pan.add(température, FlowLayout.LEFT);
+		pan.add(choixTemp,FlowLayout.CENTER );
+		pan.add(bGo, FlowLayout.RIGHT);
+		
+		pan2.setLayout(new BoxLayout(pan2,BoxLayout.PAGE_AXIS));
+		
+		if(porte == true){
+			pan2.add(imgPorteOuverte);
+		}
+		else{
+			pan2.add(imgPorteFermée);
+		}
+		
+		pan2.add(labelHumidité);
+		pan2.add(labelDerHum);
+		pan2.add(labelTempInt);
+		pan2.add(labelDerTempInt);
+		pan2.add(labelTempExt);
+		pan2.add(labelDerTempExt);
+				
+		if(pointRosée==true){
+			pan3.add(imgAlerte);
+			pan3.add(condensation);
+		}
+		
+		fenetre.add(pan,BorderLayout.PAGE_START);
+		fenetre.add(pan2,BorderLayout.EAST);
+		fenetre.add(pan3, BorderLayout.CENTER);
+		
+	}
+		
+	
+	public JButton getbGo() {
+		return bGo;
+	}
+
+	public void setbGo(JButton bGo) {
+		this.bGo = bGo;
+	}
+
+	public JLabel getLabelDerTempExt() {
+		return labelDerTempExt;
+	}
+
+
+	public void setLabelDerTempExt(JLabel labelDerTempExt) {
+		this.labelDerTempExt = labelDerTempExt;
+	}
+
+
+	public JLabel getLabelDerTempInt() {
+		return labelDerTempInt;
+	}
+
+
+	public void setLabelDerTempInt(JLabel labelDerTempInt) {
+		this.labelDerTempInt = labelDerTempInt;
+	}
+
+
+	public JLabel getLabelDerHum() {
+		return labelDerHum;
+	}
+
+
+	public void setLabelDerHum(JLabel labelDerHum) {
+		this.labelDerHum = labelDerHum;
+	}
+
+
+	public float getTempInt() {
+		return tempInt;
+	}
+
+	public void setTempInt(float tempInt) {
+		this.tempInt = tempInt;
+	}
+
+	public float getTempExt() {
+		return tempExt;
+	}
+
+	public void setTempExt(float tempExt) {
+		this.tempExt = tempExt;
+	}
+
+	public float getHumidité() {
+		return humidité;
+	}
+
+	public void setHumidité(float humidité) {
+		this.humidité = humidité;
+	}
+
+	public Boolean getPointRosée() {
+		return pointRosée;
+	}
+
+	public void setPointRosée(Boolean pointRosée) {
+		this.pointRosée = pointRosée;
+	}
+
+	public Boolean getPorte() {
+		return porte;
+	}
+
+	public void setPorte(Boolean porte) {
+		this.porte = porte;
+	}
+
+	public JLabel getTempérature() {
+		return température;
+	}
+
+	public void setTempérature(JLabel température) {
+		this.température = température;
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
